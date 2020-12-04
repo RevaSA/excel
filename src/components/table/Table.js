298822f502c806457f743ea1b7151b2e8f4f1772
Stanislav Reva <RevaSA95@gmail.com>
2020-12-04 14:00:17 +0300

    Make selection group cells

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,7 @@ import {ExcelComponent} from '@core/ExcelComponent'
 import {$} from '@core/dom'
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
-import {isCell, shouldResize} from '@/components/table/table.functions'
+import {isCell, matrix, shouldResize} from '@/components/table/table.functions'
 import {TableSelection} from '@/components/table/TableSelection'
 
 export class Table extends ExcelComponent {
@@ -33,7 +33,15 @@ export class Table extends ExcelComponent {
         if (shouldResize(event)) {
             resizeHandler(this.$root, event)
         } else if (isCell(event)) {
-            this.selection.select($(event.target))
+            const $target = $(event.target)
+
+            if (event.shiftKey) {
+                const $cells = matrix($target, this.selection.current)
+                    .map(id => this.$root.find(`[data-id="${id}"]`))
+                this.selection.selectGroup($cells)
+            } else {
+                this.selection.select($target)
+            }
         }
     }
 }
